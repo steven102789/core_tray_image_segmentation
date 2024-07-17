f857fba8c36745d25aef62de6a1190201d22d513
steven102789 <a0983910492@gmail.com>
2024-07-17 16:53:05 +0800

    🦄 refactor(AutoEncoder.py): 將AE從原本改成多層感知機
    將autoencoder從原本conv2d改成多層感知機mlp

--- notebooks/models/AutoEncoder.py
+++ notebooks/models/AutoEncoder.py
@@ -1,29 +1,37 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.layers import Input, Dense, Flatten, Reshape, Dropout
 from tensorflow.keras.optimizers import Adam
 
-def AutoEncoder(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=3):
+def MLP_AutoEncoder(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=3):
     # 输入层
     inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    x = Flatten()(inputs)
     
     # 编码器
-    x = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    
+    # 中间的code神经元
+    code = Dense(3, activation='relu')(x)
     
     # 解码器
-    x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-    x = UpSampling2D((2, 2))(x)
+    x = Dense(128, activation='relu')(code)
+    x = Dropout(0.2)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    
+    # 恢复到图像形状
+    x = Dense(IMG_HEIGHT * IMG_WIDTH * n_classes, activation='relu')(x)
+    x = Reshape((IMG_HEIGHT, IMG_WIDTH, n_classes))(x)
     
     # 输出层
-    outputs = Conv2D(n_classes, (3, 3), activation='softmax', padding='same')(x)
+    outputs = Dense(n_classes, activation='softmax')(x)
     
     # 构建模型
     model = Model(inputs, outputs)
